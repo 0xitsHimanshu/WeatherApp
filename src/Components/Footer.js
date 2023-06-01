2dbd5abbf07fc7c6ec9dec083248a11bc7e4d764
Himanshu <kyadav.9643@gmail.com>
2023-06-01 15:03:45 +0530

    Update Footer.js

---
+++ src/Components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Footer() {
+    return (
+        <div className="footer">
+            <footer class="py-5 bg-dark fixed-bottom">
+                <div class="container">
+                    <p class="m-0 text-center text-white">
+                        Copyright &copy; Himanshu 2023
+                    </p>
+                </div>
+            </footer>
+        </div>
+    );
+}
