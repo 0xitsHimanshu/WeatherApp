ef3b26fffa7993ef43069e00889199f311349852
Himanshu <kyadav.9643@gmail.com>
2023-06-01 14:35:33 +0530

    made some changes for importing Routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import React from "react";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Navigation from './Components/Navigation';
-import Weather from './Components/Weather';
-import logo from './logo.svg';
-import './App.css';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import {fab} from '@fortawesome/free-brands-svg-icons';
+import Weather from "./Components/Weather";
+import "./App.css";
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from "@fortawesome/free-brands-svg-icons"
 
 library.add(fab);
 
 function App() {
   return (
-   <React.Fragment>
+    <React.Fragment>
       <Router>
         <Navigation />
-        <Switch>
-          <Route path="/" component={()=><div className='container1'><Weather/></div>} />
+        <Routes>  
+          <Route path="/" element={() => <div className="container1"><Weather /></div>} />
           {/* <Redirect from='*' to='/' /> */}
-        </Switch>
+        </Routes>
       </Router>
-   </React.Fragment>
+    </React.Fragment>
   );
 }
 
