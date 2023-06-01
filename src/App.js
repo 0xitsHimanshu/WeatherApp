196782744f69a2910a792bca449f8f44f7bf794d
Himanshu <kyadav.9643@gmail.com>
2023-06-01 14:15:10 +0530

    added all the files
    All the files that are needed like Component folder having all the components to render and added all the images for the background/icons

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import Navigation from './Components/Navigation';
+import Weather from './Components/Weather';
 import logo from './logo.svg';
 import './App.css';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {fab} from '@fortawesome/free-brands-svg-icons';
+
+library.add(fab);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <React.Fragment>
+      <Router>
+        <Navigation />
+        <Switch>
+          <Route path="/" component={()=><div className='container1'><Weather/></div>} />
+          {/* <Redirect from='*' to='/' /> */}
+        </Switch>
+      </Router>
+   </React.Fragment>
   );
 }
 
