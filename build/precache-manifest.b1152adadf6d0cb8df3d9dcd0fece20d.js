61330f0122b6b08999ac536a53a9aee0264fe065
Himanshu <kyadav.9643@gmail.com>
2023-06-06 16:38:21 +0530

    updated

---
+++ build/precache-manifest.b1152adadf6d0cb8df3d9dcd0fece20d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f15b950e30b7bfe50a025a3d32b2c4e",
+    "url": "/WeatherApp/index.html"
+  },
+  {
+    "revision": "c8f3dd2170630e847ba8",
+    "url": "/WeatherApp/static/css/main.21b790f8.chunk.css"
+  },
+  {
+    "revision": "898563a14707e112a105",
+    "url": "/WeatherApp/static/js/2.e35d3f0a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/WeatherApp/static/js/2.e35d3f0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8f3dd2170630e847ba8",
+    "url": "/WeatherApp/static/js/main.79115426.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/WeatherApp/static/js/main.79115426.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "910599553370ba181e0e",
+    "url": "/WeatherApp/static/js/runtime-main.ee1b26f7.js"
+  },
+  {
+    "revision": "0a218296b39a3569fe9c933e3ababb49",
+    "url": "/WeatherApp/static/media/WeatherIcons.0a218296.gif"
+  },
+  {
+    "revision": "75e0f8ca422777deab777d3abe0a90e7",
+    "url": "/WeatherApp/static/media/background.75e0f8ca.jpg"
+  },
+  {
+    "revision": "e1698332435d6a6fb48294f85157aeab",
+    "url": "/WeatherApp/static/media/city.e1698332.jpg"
+  }
+]);
